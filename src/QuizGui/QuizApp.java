ae275bb06f2f95f2da33122172d12dd6552cd30d
Alex <alexander.zeljic@yh.nackademin.se>
2025-11-24 12:24:04 +0100

    QuizApp bild

--- src/QuizGui/QuizApp.java
+++ src/QuizGui/QuizApp.java
@@ -1,6 +1,13 @@
 package QuizGui;
 
+import javax.swing.*;
+
 public class QuizApp {
+    public static final String BILD = "/QuizGui/img.png";
 
-
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {QuizWindow w = new QuizWindow("Quiz-kampen", BILD);
+        w.setVisible(true);
+        });
+    }
 }
